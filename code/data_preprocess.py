d739a8bc710c4e202c7cdae384a435b012073a27
Steven <liu.congxin@watchingvision.com>
2018-04-15 19:04:44 +0800

    init

---
+++ code/data_preprocess.py
@@ -0,0 +1,88 @@
+
+from mlog import initlog
+import glob
+import os
+import numpy as np
+import logging
+import cv2
+import math
+import cPickle as pickle
+
+
+def read_image_mask(img_folder):
+    img_fname = glob.glob(os.path.join(img_folder, "images", "*.png"))[0]
+    img = cv2.imread(img_fname)
+
+    mask_fnames = glob.glob(os.path.join(img_folder, "masks", "*.png"))
+    masks = np.array([cv2.imread(x)[:, :, 0] for x in mask_fnames])
+    mask = (np.sum(masks, axis=0) > 0).astype(np.uint8)
+
+    return img, mask
+
+
+def calc_parts(x, s):
+    n_parts = int(math.ceil(x/float(s)))
+    parts = []
+    for i in range(n_parts):
+        if i == n_parts-1:
+            p = x - s
+        else:
+            p = i * s
+        parts.append(p)
+
+    return parts
+
+
+def split_parts(img, mask, s=(256,256)):
+    assert(img.shape[0] == mask.shape[0] and img.shape[1] == mask.shape[1])
+
+    if img.shape[0] < s[0] or img.shape[1] < s[1]:
+        raise ValueError("image shape is less than %d" % s)
+
+    h_parts = calc_parts(img.shape[0], s[0])
+    w_parts = calc_parts(img.shape[1], s[1])
+
+    img_parts = []
+    mask_parts = []
+    parts = []
+
+    for h in h_parts:
+        for w in w_parts:
+            img_parts.append(img[h:h+s[0], w:w+s[1]])
+            mask_parts.append(mask[h:h+s[0], w:w+s[1]])
+            parts.append([h, w])
+
+    return img_parts, mask_parts, parts
+
+
+
+def get_dataset(folder, n_samples=None):
+    imgs_folders = glob.glob(os.path.join(folder, "*"))[:n_samples]
+    ds = {}
+
+    k = 0
+    for i, f in enumerate(imgs_folders):
+        img, mask = read_image_mask(f)
+        img = img[:, :, 0]    # for gray scale
+        img_parts, mask_parts, parts = split_parts(img, mask, (256,256))
+        data = [{'pos': x[2], 'img': x[0], 'mask': x[1], 'raw_shape': img.shape} for x in zip(img_parts, mask_parts, parts)]
+        img_id = os.path.basename(f)
+        ds[img_id] = data
+        k += len(data)
+        logging.info("Precess folder: %s, shape: %s, parts: %s", f, img.shape, len(data))
+
+    logging.info("totol read, image: %d parts: %s", len(imgs_folders), k )
+    return ds
+
+def main():
+    data_dir = "data/datasets/stage1_train"
+    ds = get_dataset(data_dir, n_samples=None)
+
+    pkl_fname = "data/preprocess/stage1_train_set.pkl"
+    with open(pkl_fname, "wb") as f:
+        pickle.dump(ds, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    initlog()
+    main()
---
+++ code/train.py
@@ -0,0 +1,141 @@
+
+
+from mlog import initlog
+import glob
+import os
+import numpy as np
+import logging
+import cv2
+import math
+import cPickle as pickle
+import matplotlib.pylab as plt
+
+from tf_unet import image_gen
+from tf_unet import unet
+from tf_unet import util
+from tf_unet import image_util
+
+def get_dataset(pkl_fname):
+    with open(pkl_fname, "rb") as f:
+        ds = pickle.load(f)
+
+    n_parts = sum(len(v) for v in ds.values())
+    logging.info("n_parts: %d", n_parts)
+
+    images = []
+    masks = []
+
+    n_err = 0
+    for key, value in ds.items():
+        for i, d in enumerate(value):
+            if np.max(d['img']) == 0:
+                logging.info('skip: %s, # %s', key, i)
+                n_err += 1
+                continue
+            images.append(d['img'])
+            masks.append(d['mask'])
+
+    images = np.array(images, dtype=np.float32)
+    masks = np.array(masks, dtype=np.bool)
+    max_v = np.max(images)
+    if max_v > 0.0:
+        images /= max_v
+
+    logging.info("valid images: #%d, err: #%d", len(images), n_err)
+    return images, masks
+
+
+def test_data_provider(data_provider):
+    x_test, y_test = data_provider(1)
+
+    fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(12, 5))
+    ax[0].imshow(x_test[0, ..., 0], aspect="auto")
+    gt = y_test[0, ..., 1]
+    # mask = prediction[0, ..., 1] > 0.5
+    # gt_cropped = crop_to_shape(gt, mask.shape)
+    ax[1].imshow(gt, aspect="auto")
+    #ax[2].imshow(mask, aspect="auto")
+    ax[0].set_title("Input")
+    ax[1].set_title("Ground truth")
+    #ax[2].set_title("Prediction")
+    logging.info("test_data_provider:: image:[%s, %s], mask:[%s, %s]", np.max(x_test[0, ..., 0]), np.min(x_test[0, ..., 0]), np.max(gt), np.min(gt))
+    fig.tight_layout()
+    plt.show()
+
+
+def test_data(images, masks, i):
+    fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(12, 5))
+    ax[0].imshow(images[i, ...], aspect="auto")
+    gt = masks[i, ...]
+    # mask = prediction[0, ..., 1] > 0.5
+    # gt_cropped = crop_to_shape(gt, mask.shape)
+    ax[1].imshow(gt, aspect="auto")
+    #ax[2].imshow(mask, aspect="auto")
+    ax[0].set_title("Input")
+    ax[1].set_title("Ground truth")
+    #ax[2].set_title("Prediction")
+    fig.tight_layout()
+    plt.show()
+
+def padding_array(ary, offset, default_val):
+    logging.info("ary.shape: %s", ary.shape)
+    p1 = offset/2
+    p2 = offset - p1
+
+    ary_padded = np.pad(ary, ((0, 0), (p1, p2),(p1, p2)), "constant", constant_values=default_val)
+
+    return ary_padded
+
+
+
+def unet_size(s, L):
+    s0 = s
+    for i in range(L - 1):
+        s = (s - 4) / 2
+        print("layer %d, %d" % (i + 1, s))
+    s -= 4
+    print("Layer %d, %d" % (L, s))
+    for i in range(L - 2, -1, -1):
+        s = s * 2 - 4
+        print("Layer %d, %d" % (i + 1, s))
+
+    return s, s0 - s
+
+def main():
+    LAYERS = 3
+    pkl_fname = "data/preprocess/stage1_train_set.pkl"
+    images, masks = get_dataset(pkl_fname)
+    logging.info("read train set: %s, %s", images.shape, masks.shape)
+    logging.info("image:[%s, %s], mask:[%s, %s]", np.max(images), np.min(images), np.max(masks), np.min(masks))
+
+    pred_size, offset = unet_size(256, LAYERS)
+    logging.info("pred_size: %d, offset: %d", pred_size, offset)
+    images = padding_array(images, offset, default_val=0.0)
+    masks = padding_array(masks, offset, default_val=False)
+    logging.info("shape after padded: %s, %s", images.shape, masks.shape)
+
+
+    # test_data(images, masks, 1679)
+    data_provider = image_util.SimpleDataProvider(images, masks)
+    logging.info("data_provider.channels: %s, data_provider.n_class: %s", data_provider.channels, data_provider.n_class)
+
+    # test_data_provider(data_provider)
+    net = unet.Unet(channels=data_provider.channels,
+                    n_class=data_provider.n_class,
+                    cost='cross_entropy',
+                    layers=LAYERS,
+                    features_root=64,
+                    cost_kwargs=dict(regularizer=0.001),
+                    )
+    batch_size = 8
+    training_iters = (images.shape[0]-1) / batch_size + 1
+    logging.info("batch_size: %s, iters: %s", batch_size, training_iters)
+
+    trainer = unet.Trainer(net, batch_size=batch_size, optimizer="momentum",
+                           opt_kwargs=dict(momentum=0.8, learning_rate=0.01))
+    path = trainer.train(data_provider, "log/20180414",
+                         training_iters=training_iters, epochs=30, display_step=2)
+
+if __name__ == "__main__":
+    initlog()
+    main()
