3487cc2294946e2ccadb68708f843f409306dc59
Steven <liu.congxin@watchingvision.com>
2018-04-15 19:55:16 +0800

    new

--- code/eval.py
+++ code/eval.py
@@ -25,7 +25,7 @@ def crop_to_shape(data, shape):
     """
     offset0 = (data.shape[0] - shape[0])//2
     offset1 = (data.shape[1] - shape[1])//2
-    return data[offset0:(-offset0), offset1:(-offset1)]
+    return data[offset0:shape[0]+offset0, offset1:shape[1]+offset1]
 
 def calc_dice(pred, gt):
     print(pred.shape, gt.shape)
@@ -44,21 +44,27 @@ def calc_dice(pred, gt):
 
 
 def main():
-	LAYERS = 3
-    pkl_fname = "data/preprocess/stage1_train_set.pkl"
-    images, masks = get_dataset(pkl_fname)
-    logging.info("read train set: %s, %s", images.shape, masks.shape)
-    logging.info("image:[%s, %s], mask:[%s, %s]", np.max(images), np.min(images), np.max(masks), np.min(masks))
+    np.random.seed(12345)
+    LAYERS = 3
+    padding_pkl_fname = "data/preprocess/stage1_train_set_padding.pkl"
 
-    pred_size, offset = unet_size(256, LAYERS)
-    logging.info("pred_size: %d, offset: %d", pred_size, offset)
-    images = padding_array(images, offset, default_val=0.0)
-    masks = padding_array(masks, offset, default_val=False)
-    logging.info("shape after padded: %s, %s", images.shape, masks.shape)
-	pkl_fname = "data/preprocess/stage1_train_set_padding.pkl"
-    with open(pkl_fname, "wb") as f:
-    	pickle((images, masks), f, protocol=pickle.HIGHEST_PROTOCOL)
+    dump = False
+    if dump:
+        pkl_fname = "data/preprocess/stage1_train_set.pkl"
+        images, masks = get_dataset(pkl_fname)
+        logging.info("read train set: %s, %s", images.shape, masks.shape)
+        logging.info("image:[%s, %s], mask:[%s, %s]", np.max(images), np.min(images), np.max(masks), np.min(masks))
 
+        pred_size, offset = unet_size(256, LAYERS)
+        logging.info("pred_size: %d, offset: %d", pred_size, offset)
+        images = padding_array(images, offset, default_val=0.0)
+        masks = padding_array(masks, offset, default_val=False)
+        logging.info("shape after padded: %s, %s", images.shape, masks.shape)
+        with open(padding_pkl_fname, "wb") as f:
+            pickle.dump((images, masks), f, protocol=pickle.HIGHEST_PROTOCOL)
+    else:
+        with open(padding_pkl_fname, "rb") as f:
+            images, masks = pickle.load(f)
 
     # test_data(images, masks, 1679)
     data_provider = image_util.SimpleDataProvider(images, masks)
@@ -73,23 +79,27 @@ def main():
                     cost_kwargs=dict(regularizer=0.001),
                     )
 
-	x_test, y_test = data_provider(1)
-	prediction = net.predict("log/20180414/model.cpkt", x_test)
-	mask = prediction[0,...,1] > 0.5
-	img_cropped = crop_to_shape(x_test[0,...,0], mask.shape)
-	gt = y_test[0,...,1]
-	gt_cropped = crop_to_shape(gt, mask.shape)
+    for i in range(5):
+        x_test, y_test = data_provider(1)
+        prediction = net.predict("log/20180414/model.cpkt", x_test)
+        logging.info("%s, %s", x_test.shape, y_test.shape)
+        mask = prediction[0,...,1] > 0.3
+        img_cropped = crop_to_shape(x_test[0,...,0], mask.shape)
+        gt = y_test[0,...,1]
+        gt_cropped = crop_to_shape(gt, mask.shape)
 
-	fig, ax = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(12,5))
-	ax[0].imshow(img_cropped, aspect="auto")
+        fig, ax = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(12,5))
+        ax[0].imshow(img_cropped, aspect="auto")
 
-	ax[1].imshow(gt_cropped, aspect="auto")
-	ax[2].imshow(mask, aspect="auto")
-	ax[0].set_title("Input")
-	ax[1].set_title("Ground truth")
-	ax[2].set_title("Prediction")
-	fig.tight_layout()
-	logging.info("dice: %f", calc_dice(mask, gt_cropped))
+        ax[1].imshow(gt_cropped, aspect="auto")
+        ax[2].imshow(mask, aspect="auto")
+        ax[0].set_title("Input")
+        ax[1].set_title("Ground truth")
+        ax[2].set_title("Prediction")
+        fig.tight_layout()
+        logging.info("%s, %s", mask.shape, gt_cropped.shape)
+        logging.info("dice: %f", calc_dice(mask, gt_cropped))
+        plt.show()
 
 
 if __name__ == "__main__":
